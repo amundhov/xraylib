b38fa19a6a22c2ae5432a8cb43fa55a27abe30cc
Amund Hov <hov@esrf.fr>
2012-06-29 15:46:12 +0200

    Add utility class for constants

--- src/xraylib/common.py
+++ src/xraylib/common.py
@@ -1,5 +1,17 @@
 import scipy.io
-import numpy
+import numpy as np
+
+class Bunch:
+    def __init__(self, **kwds):
+        self.add(**kwds)
+    def add(self, **kwds):
+        self.__dict__.update(kwds)
+    def __repr__(self):
+        return unicode(self)
+    def __unicode__(self):
+        format_str = u": %s\n".join(self.__dict__.keys())
+        format_str +=u": %s\n"
+        return format_str % tuple(self.__dict__.values())
 
 #
 # XrayTable contains various data related to interactions with X-rays for a
@@ -9,9 +21,10 @@ import numpy
 # >>> xraylib.Xraytable['Density'][1] # density of Hydrogen
 # >>> xraylib.Xraytable[1]['Density'] # also density of Hydrogen
 #
+
 XrayTable = scipy.io.loadmat('../data/xraytable.mat', squeeze_me=True)['XrayTable']
 # Fake '1'-indexing of table to match atomic number
-XrayTable = numpy.hstack((numpy.array(numpy.zeros(1),dtype=XrayTable.dtype),XrayTable))
+XrayTable = np.hstack((np.array(np.zeros(1),dtype=XrayTable.dtype),XrayTable))
 
 _Elements  = scipy.io.loadmat('../data/elements.mat', squeeze_me=True)['elements']
 Elements = {}
@@ -20,21 +33,24 @@ for element in _Elements:
     Elements[element.strip()] = _count
     _count += 1
 
-Constants = {
-    'c' : 299792458;        # (m/s) speed of light in vacuum (exact)
-    'mu':   4.0e-7*pi;      # (N/A^2) magnetic constant mu_0 (exact)
-    'Na':   6.0221415e23;   # Avogadro constants
-    'kB':   1.3806505e-23;  # (J/K) Boltzmann constant
-    'h' :   6.6260693e-34;  # (Js) Planck constnat
-    'G' :   6.6742e-11;     # (m^3/kg/s^2) gravitation constant
-    'e' :   1.60217653e-19; # (J) electron volt
-    'me': 510.998918;       # electron mass in keV
 
-    'ep': 1/(c.mu*c.c^2);         # (F/m) electric constant eps_0
-    'hc': 1e7*c.h*c.c/c.e;        # (keV A) hc
-    're': c.c^2*c.e/c.me;         # classical radius of electron in A
-    'ia': 2*c.h/(c.e^2*c.mu*c.c); # 1/fine-structure
-}
+Constants = Bunch(
+    c  = 299792458,        # (m/s) speed of light in vacuum (exact)
+    mu =   4.0e-7*np.pi,   # (N/A^2) magnetic constant mu_0 (exact)
+    Na =   6.0221415e23,   # Avogadro constants
+    kB =   1.3806505e-23,  # (J/K) Boltzmann constant
+    h  =   6.6260693e-34,  # (Js) Planck constnat
+    G  =   6.6742e-11,     # (m^3/kg/s^2) gravitation constant
+    e  =   1.60217653e-19, # (J) electron volt
+    me = 510.998918        # electron mass in keV
+)
+
+Constants.add(
+    ep = 1/(Constants.mu*Constants.c**2),         # (F/m) electric constant eps_0
+    hc = 1e7*Constants.h*Constants.c/Constants.e,        # (keV A) hc
+    re = Constants.c**2*Constants.e/Constants.me,         # classical radius of electron in A
+    ia = 2*Constants.h/(Constants.e**2*Constants.mu*Constants.c)  # 1/fine-structure
+)
 
 def strtoz(string):
     ''' Converts a string of stoichiometry into a dictionary 
