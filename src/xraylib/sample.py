8815e3a158abc23512f786323abf5709a04f0ddd
Amund Hov <hov@esrf.fr>
2012-08-08 19:13:34 +0200

    Start work on quantities weighted by stoichiometry

--- src/xraylib/sample.py
+++ src/xraylib/sample.py
@@ -30,5 +30,20 @@ class Sample:
         return _attribute_list()
     def _attribute_list(self):
         return [getattr(self,o) for o in Sample.fields]
+    def mass_attenuation_coefficient(self,E):
+        cross_section,mass = weighted(crossection.get_cross_section)(self,E)
+        return total_cross_section*(Constants.Na*1e-24/mass)
 
+
+def weighted(fun):
+    ''' Decorator for calculations which are to be
+        weighted according to sample stoichiometry. '''
+    def wrapped_fun(sample,*_args,**_kwargs):
+        Z = [ o[1] for o in sample.compound[1].keys() ]
+        weights = sample.compound[1].values()
+        res = 0
+        for i in xrange(0,len(weights)):
+            res += weights[i]*fun(z=Z[i],*_args,**_kwargs)
+        return res
+    return wrapped_fun
 
