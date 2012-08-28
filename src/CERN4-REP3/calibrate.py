3f57e89b8e4fb1e4eb79ef107e93d996d687f802
Amund Hov <hov@esrf.fr>
2012-08-28 20:32:58 +0200

    Add missing py files - calibration and dark current subtraction

---
+++ src/CERN4-REP3/calibrate.py
@@ -0,0 +1,44 @@
+from numpy import *
+
+#  # HORRIBLE HACK MUST DIE
+#  import os, sys, inspect
+#  cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+#  #cmd_folder = '/'.join(cmd_folder.split('/')[:-1])
+#  if cmd_folder not in sys.path:
+#      sys.path.insert(0, cmd_folder)
+#      print('FIXME: %s adding %s to system path'%(__name__,cmd_folder))
+
+import xraylib, os
+from xraylib import f2w  
+
+try:
+    file_name
+    file_path
+except NameError:
+    file_name = 'CeO2-900mm-90keV1.h5'
+    file_path = '/mntdirect/_users/hov/workspace/xray/output/CERN4-REP3/CeO2-900mm-90keV1.h5'
+
+try:
+    image
+except NameError:
+    # image not given, open calibration file manually
+    extension = os.path.splitext(file_name)[1]
+    print extension
+    if extension == '.edf':
+        import EdfFile
+        image = EdfFile(file_path).GetData(0)
+    elif extension == '.h5':
+        import h5py
+        file = h5py.File(file_path)
+        image = file['/entry/image'].value
+        image = image.squeeze()
+
+# Simple image thresholding to reduce noise
+image[image<100] = 0
+
+det = f2w.Perkin()
+det.calibrate(image,[10,350])
+
+print(det)
+
+parms = unicode(det)
---
+++ src/CERN4-REP3/dark_current.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+import xraylib, os
+from xraylib import f2w,files
+
+try:
+    base_name
+    directory = file_dir
+except NameError:
+    #FIXME MOCK PARAMETERS - intregrate with workbench
+    base_name = 'DARK_CERN4-REP3_000'
+    directory = '/data/id15/inhouse/Olof/CERN4-REP3_000/'
+
+files = [ files.Image(directory,o) for o in os.listdir(directory) if o.startswith(base_name) ]
+
+image = files[0].getImage().squeeze()
+images = np.zeros((len(files),) + image.shape)
+
+print(images.shape)
+
+from time import time
+start = time()
+
+for i in xrange(0,len(files)):
+    images[i] = files[i].getImage()
+    print('%s of %s images loaded' % (i+1,len(files),))
+
+print('%s seconds used' % (time()-start))
+
+det = f2w.Perkin()
+det.setorigin(np.array([ 197.20634855,  211.88283524]))
+det.settilt(np.array([ -0.18697419,  0.0135755]))
+
+print('--> Performing median')
+start = time()
+dark_current = np.median(images,axis=0)
+print('%s seconds used' % (time()-start))
+
+print('--> Integrating ')
+start = time()
+(radius, amplitude)= det.integrate(dark_current)
+radius = radius.flatten()
+amplitude = amplitude.flatten()
+print('%s seconds used' % (time()-start))
