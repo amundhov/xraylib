a020d2b62ce762c9d67d779bc3d53ec87723b9d4
Amund Hov <hov@esrf.fr>
2013-04-11 13:56:55 +0200

    Use fabio for Files utility

--- xraylib/xraylib/files.py
+++ xraylib/xraylib/files.py
@@ -1,22 +1,34 @@
 import os
 
-class Image:
-    def __init__(self,directory,file_name,resource_path='/entry/image'):
-        self._extension = os.path.splitext(file_name)[1]
-        self._file_path = directory+file_name
-        self._resource_path = resource_path
-    def getImage(self):
-        if self._extension == '.edf':
-            import EdfFile
-            image = EdfFile.EdfFile(self._file_path).GetData(0)
-        elif self._extension == '.h5':
-            import h5py
-            file = h5py.File(self._file_path)
-            image = file[self._resource_path].value
-        else:
-            raise Exception('Unknown file extension %s' % (self._extension,))
-        return image
+import fabio
 
-    def saveImage(self):
-        ''' TODO: implement me '''
-        pass
+class Image:
+    def __init__(self,file_path, data_set='/entry/image'):
+        self._extension = os.path.splitext(file_path)[1]
+        self._file_path = file_path
+        self._data_set = data_set
+    def getImage(self):
+        if self._extension == '.h5':
+            import h5py
+            with h5py.File(self._file_path) as f:
+                self.image = f[self._data_set].value
+        else:
+            self.image = fabio.openimage.openimage(self._file_path).data
+        return self.image
+
+    def saveImage(self, image, data_path='/xraylib/image'):
+        """ TODO: implement me """
+        if self._extension == '.h5':
+            with h5py.File(self.hdf5) as f:
+                group = f.require_group(os.path.dirname(self.data_path))
+
+                if os.path.basename(self.data_path) in self.group:
+                    self.dataset = self.group[os.path.basename(self.data_path)]
+                else:
+                    self.dataset = self.group.create_dataset(
+                        name=os.path.basename(self.data_path),
+                        shape=image.shape,
+                        dtype=image.dtype,
+                        # TODO, take chunks as parameter
+                        chunks=(1, self.nTrans, self.nDiff),
+                        maxshape=(None, None, self.nDiff))
