780957d9972f0a6b74034a1e63418acd7cdc96b8
Amund Hov <amundhov@samfundet.no>
2013-04-25 16:04:52 +0200

    Move averaging of images to utils

--- xraylib/xraylib/utils.py
+++ xraylib/xraylib/utils.py
@@ -1,3 +1,8 @@
+import os
+import xraylib
+import numpy as np
+from xraylib import files
+
 def strip_none_values(dictionary):
     return dict([ (o,v) for o,v in dictionary.items() if v is not None])
 
@@ -8,3 +13,12 @@ def toFloat(val):
         return float(str(val).strip())
     except ValueError:
         return None
+
+def averageImages(filenames, method='median', verbose=False):
+    """ Load and average a list of images. """
+    images = [files.ImageFile(f).getImage() for f in filenames if os.path.isfile(f)]
+    images = np.array(images)
+    if method == 'median':
+        return np.median(images,axis=0)
+    else:
+        raise Exception('NOT IMPLEMENTED')
