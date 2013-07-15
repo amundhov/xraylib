3b23148527a018dabd1f79b1c21b17f42abe5d1c
Amund Hov <amundhov@samfundet.no>
2013-07-15 14:59:16 +0200

    Don't subtract integrated darkcurrent. Let pyFAI do what it thinks is best

--- xraylib/xraylib/__init__.py
+++ xraylib/xraylib/__init__.py
@@ -1,6 +1,7 @@
 IMAGE_PATH = '/entry/image' # default path used by DAWN
 AVERAGE_DATA_SET = '/xraylib/average'
 DARKCURRENT_DATA_SET = '/xraylib/darkcurrent'
+TWO_THETA_DATA_SET = '/xraylib/two_theta'
 CALIBRATION_IMAGE = '/xraylib/calibration_image'
 CALIBRATION_PROFILE = '/xraylib/calibration_profile'
 DIFFRACTOGRAM_DATA_SET = '/xraylib/diffractogram'
