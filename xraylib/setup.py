57606de5a13e9c68d9db5d98deb7ed091588ba28
Amund Hov <amundhov@samfundet.no>
2013-08-08 23:07:58 +0200

    Add h5py fabio and pyFAI to requirements

--- xraylib/setup.py
+++ xraylib/setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
 import os, sys, glob
 
 script_files = glob.glob("scripts/*")
+install_requires=[ o.strip('\n') for o in open('requirements.txt').readlines() if not '#' in o]
 
 setup(name='xraylib',
       version='0.9',
       description='Xraylib provides routines for calculating cross-sections, form factors and other quantities commonly needed in X-ray experiments',
+      install_requires=install_requires,
       package_data = { 'xraylib' : ['data/*.pickle'], },
       include_package_data=True,
       packages=['xraylib'],
       scripts=script_files,
       test_suite='tests',
-      install_requires=['numpy'],
       author='Amund Hov',
       author_email='amund.hov@esrf.fr',
       url='http://www.python.org/sigs/distutils-sig/',
